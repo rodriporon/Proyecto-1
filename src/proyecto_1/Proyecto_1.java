a987c65370cab35f729ad2efaff90358d67328b5
Rodri Porón <201902781@ingenieria.usac.edu.gt>
2020-09-03 22:11:16 -0600

    first commit

---
+++ src/proyecto_1/Proyecto_1.java
@@ -0,0 +1,19 @@
+package proyecto_1;
+
+/**
+ *
+ * @author Rodrigo Porón
+ */
+public class Proyecto_1 {
+    
+    public static String[] datos = new String[10];
+    public static int contador = 1;
+    public static Usuario[] usuarios = new Usuario[10];
+    // public static Clientes[] clientes = new Clientes[100];
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
---
+++ src/proyecto_1/Usuario.java
@@ -0,0 +1,57 @@
+package proyecto_1;
+
+/**
+ *
+ * @author Rodrigo Porón
+ */
+public class Usuario {
+    
+    private String nombre;
+    private String user;
+    private String pass;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the pass
+     */
+    public String getPass() {
+        return pass;
+    }
+
+    /**
+     * @param pass the pass to set
+     */
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    
+    
+}
